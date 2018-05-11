ca09c7cd782c862c23f7b0d6ddb5507ffb2b68cc
Dennis Ruto <rutodeno@gmail.com>
2018-05-10 21:11:36 -0400

    setup connection to database

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+
+});
+
+connection.connect(function(err){
+    if(err){
+        console.log("error connection: "+ err.stack);
+        return;
+    }
+
+    console.log("connected as id: "+ connection.threadId);
+})
+
+module.exports = connection ;
