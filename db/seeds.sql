0134159f595297e9e08824a285fcb55f221f5965
Dennis Ruto <rutodeno@gmail.com>
2018-05-10 21:35:58 -0400

    added mysql entry to schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES("Philly Burger");
+INSERT INTO burgers (burger_name) VALUES("Guacamol Burger");
+INSERT INTO burgers (burger_name) VALUES("Vegeterian Burger");
