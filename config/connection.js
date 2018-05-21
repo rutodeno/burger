699044505285949973d42a0964c8be037c4761e7
Dennis Ruto <rutodeno@gmail.com>
2018-05-20 20:22:56 -0400

    added connection to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,23 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "Kipsang1990",
-    database: "burgers_db"
+var connection ;
+if (process.env.JAWSDB_URL) {
 
-});
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "Mungu@sifiWE20!8",
+        database: "burgers_db"
+    
+    });
+
+}
+
 
 connection.connect(function(err){
     if(err){
